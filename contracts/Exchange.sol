c5fffb490aff284b084e0e38d8a590f99bdee56f
Seyedmohammadhossein Razavi <mohammad@Seyedmohammadhosseins-MacBook-Pro.local>
2022-10-21 11:44:50 +0100

    11. Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -12,16 +12,38 @@ contract Exchange {
 	 //how mmany tokens each userdeposited tracking
 	 // token address , user address, howmany deposited
 	 mapping(address => mapping(address => uint256)) public tokens;
+
+	 //creating mapping for order 
+	 mapping(uint256 => _Order) public orders;
+	 uint256 public orderCount; // for id 
+ 	
 	 //event for deposit 
 	 event Deposit(address token, address user, uint256 amount, uint256 balance);
+	 
 	 //event for withhdraw
 	 event Withdraw(address token, address user, uint256 amount, uint256 balance);
+
+	 //event for order 
+	 event Order(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+
 
 	constructor(address _feeAccount, uint256 _feePercent){
 		feeAccount = _feeAccount;
 		feePercent =_feePercent;
 
     }
+
+    //struct for orders, a way to model the order 
+    struct _Order{
+    	//Attributes of an order 
+    	uint256 id; //unique identifier for order  
+    	address user; // track the user who made order 
+    	address tokenGet; // address of token they recievce 
+    	uint256 amountGet; // ampont of token they receive
+    	address tokenGive; // address of toke they give
+    	uint256 amountGive; // amount they give 
+    	uint256 timestamp; // when the order was created  
+    } 
 
 	//---------------------Deposite tokes------------------------
 	function depositToken(address _token, uint256 _amount) public{
@@ -58,5 +80,33 @@ contract Exchange {
 
 	}
 
+	//---------------------Making Order------------------------
 
+	//Token Give (the token they want to spend)- whichh token , how much 
+	//Token Get(the token they want to recieve)- whichh token , how much 
+	function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+		//require token balance
+		require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+		//creating order 
+		orderCount = orderCount +1;
+		orders[orderCount] = _Order(orderCount, //user id
+		 msg.sender, // user address
+		 _tokenGet, //tokenGet
+		 _amountGet, // amountGte
+		 _tokenGive, // tokenGive
+		 _amountGive, // amountGive
+		 block.timestamp // timestamp whhich is second and 
+		);
+
+		//emit event
+		emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp); 
+
+
+
+	}
 }
+
+
+
+
+
--- test/Exchange.js
+++ test/Exchange.js
@@ -12,7 +12,7 @@ describe('Exchange', ()=>{
 
 
 	// preventing any duplicate + factorising let token 
-	let deployer, feeAccount , exchange , user1 , token1, transaction
+	let deployer, feeAccount , exchange , user1 , token1, transaction, token2
 	// let accounts
 	// let deployer
 	const feePercent = 10
@@ -22,6 +22,7 @@ describe('Exchange', ()=>{
 		const Token = await ethers.getContractFactory('Token')
 		//token 1
 		token1 = await Token.deploy('DAPP University', 'DAPP','1000000')
+		token2= await Token.deploy('Mock Dai', 'mDAI','1000000')
 		
 
 
@@ -29,6 +30,7 @@ describe('Exchange', ()=>{
 		deployer = accounts[0] //first address from our blockchain
 		feeAccount = accounts[1]
 		user1= accounts[2]
+		
 
 		//give user some token
 		transaction = await token1.connect(deployer).transfer(user1.address, tokens(100))
@@ -41,18 +43,18 @@ describe('Exchange', ()=>{
 
     describe('Deployment', ()=>{
 	
-	it('tracks the fee account', async ()=>{
+	 it('tracks the fee account', async ()=>{
 		
 		expect(await exchange.feeAccount()).to.equal(feeAccount.address)
-	})
+	 })
 
-	it('traks thhe fee percent', async()=>{
+	 it('traks thhe fee percent', async()=>{
 
 		expect(await exchange.feePercent()).to.equal(feePercent)
-	})
+	 })
 
 
-   })
+    })
 
     //-------------------Deposite----------------------
     describe('depositing tokens', ()=>{
@@ -164,8 +166,8 @@ describe('Exchange', ()=>{
 
 
    })
-
-   describe('Checking Balances', ()=>{
+   // checking the balance (technique)
+    describe('Checking Balances', ()=>{
         let transaction, result
         let amount = tokens(1)
     	
@@ -191,6 +193,66 @@ describe('Exchange', ()=>{
     	})
 
     	
+   })
+
+   //-------------------Making orders-----------------------
+   describe('Making Order', async ()=>{
+        let transaction, result
+        let amount = tokens(1)
+        
+    	describe('Success', async()=>{
+
+
+    		beforeEach(async () =>{
+
+    			//deposite token before making order 
+
+    			// we need to approve 
+				transaction = await token1.connect(user1).approve(exchange.address, amount)
+				result = await transaction.wait()
+
+				// then we deposite
+				transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+				result = await transaction.wait()
+
+				//making order 
+				transaction = await exchange.connect(user1).makeOrder(token2.address,amount,token1.address, amount)
+				result = await transaction.wait()
+
+    			
+    			})
+
+    		it('tracks the newly created order', async() => {
+    			expect(await exchange.orderCount()).to.equal(1) 
+				
+			})
+
+			it('emit a Order event', async()=>{
+    			const event = result.events[0] // 2event emmited
+    			expect(await event.event).to.equal('Order')
+    			//check args
+			    const args = event.args
+			 	//console.log(args)
+			 	expect(args.id).to.equal(1)
+			 	expect(args.user).to.equal(user1.address)
+			 	expect(args.tokenGet).to.equal(token2.address)
+			 	expect(args.amountGet).to.equal(tokens(1))
+			 	expect(args.tokenGive).to.equal(token1.address)
+			 	expect(args.amountGive).to.equal(tokens(1))
+			 	expect(args.timestamp).to.at.least(1)
+			 })
+
+    		
+    	})
+
+    	describe('Failure', async()=>{
+    		it('rejects with no balance', async()=>{
+    			await expect(exchange.connect(user1).makeOrder(token2.address, tokens(1), token1.address, tokens(1))).to.be.reverted 
+    		})
+    		
+
+    	})
+
 
     })
 
